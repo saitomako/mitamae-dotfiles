50bb462cb6396d35e62a82515148b5e3e011618a
Makoto Saito <saitomako@gmail.com>
2022-04-01 09:06:54 +0900

    misc

--- install.sh
+++ install.sh
@@ -1,3 +1,11 @@
-#!/bin/bash
+#!/bin/sh
 
-~/bin/mitamae local --node-yaml=nodes/local.yml "$@" entrypoint.rb
+set -ex
+
+if [ ! -f config/.config/nvim/dein/repos/github.com/Shougo/dein.vim/.git ]; then
+  git submodule init && git submodule update --depth 1
+fi
+
+bin/setup
+
+
